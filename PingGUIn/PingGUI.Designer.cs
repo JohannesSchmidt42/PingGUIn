9bd4473c8b397d2e4bcd28eeb378a0fc0ba5629a
unknown <johannes.schmidt42@gmail.com>
2014-11-19 22:27:50 +0100

    First import, basic functionality is implemented

---
+++ PingGUIn/PingGUI.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace PingGUIn
+{
+    partial class PingGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PingGUI));
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.richTextBoxIp = new System.Windows.Forms.TextBox();
+            this.buttonScanSubnet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.Silver;
+            this.buttonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAdd.Location = new System.Drawing.Point(281, 12);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 21);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.BackColor = System.Drawing.Color.Silver;
+            this.buttonRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRemove.Location = new System.Drawing.Point(362, 12);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 21);
+            this.buttonRemove.TabIndex = 1;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = false;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.BackColor = System.Drawing.Color.Gray;
+            this.listView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView.FullRowSelect = true;
+            this.listView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView.Location = new System.Drawing.Point(13, 39);
+            this.listView.MultiSelect = false;
+            this.listView.Name = "listView";
+            this.listView.ShowGroups = false;
+            this.listView.Size = new System.Drawing.Size(557, 255);
+            this.listView.TabIndex = 2;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "IP";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Ping";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Width = 250;
+            // 
+            // richTextBoxIp
+            // 
+            this.richTextBoxIp.BackColor = System.Drawing.Color.Gray;
+            this.richTextBoxIp.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxIp.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxIp.Location = new System.Drawing.Point(13, 12);
+            this.richTextBoxIp.Name = "richTextBoxIp";
+            this.richTextBoxIp.Size = new System.Drawing.Size(262, 18);
+            this.richTextBoxIp.TabIndex = 3;
+            this.richTextBoxIp.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBoxIp_KeyDown);
+            // 
+            // buttonScanSubnet
+            // 
+            this.buttonScanSubnet.BackColor = System.Drawing.Color.Silver;
+            this.buttonScanSubnet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonScanSubnet.Location = new System.Drawing.Point(443, 12);
+            this.buttonScanSubnet.Name = "buttonScanSubnet";
+            this.buttonScanSubnet.Size = new System.Drawing.Size(126, 21);
+            this.buttonScanSubnet.TabIndex = 4;
+            this.buttonScanSubnet.Text = "Scan Subnet";
+            this.buttonScanSubnet.UseVisualStyleBackColor = false;
+            this.buttonScanSubnet.Click += new System.EventHandler(this.buttonScanSubnet_Click);
+            // 
+            // PingGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(581, 306);
+            this.Controls.Add(this.buttonScanSubnet);
+            this.Controls.Add(this.richTextBoxIp);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(597, 345);
+            this.Name = "PingGUI";
+            this.Text = "PingGUIn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TextBox richTextBoxIp;
+        private System.Windows.Forms.Button buttonScanSubnet;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ PingGUIn/PingGUI.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace PingGUIn
+{
+    public partial class PingGUI : Form
+    {
+        private bool _isClosing;
+        
+        public PingGUI()
+        {
+            InitializeComponent();
+            this.FormClosing += PingGUI_FormClosing;
+            this.ActiveControl = richTextBoxIp;
+        }        
+
+        #region form setup
+        void PingGUI_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _isClosing = true;
+        }
+        #endregion
+
+        private void PingHostForever(string hostNameOrIp, ListViewItem item)
+        {
+            var thread = new Thread(()=>
+            {
+                using(var pinger = new Ping())
+                {
+                    bool triedGetName = false;
+                    while((item.ListView != null) && (_isClosing == false))
+                    {
+                        PingReply reply = null;
+                        try
+                        {
+                            reply = pinger.Send(hostNameOrIp);
+                        }
+                        catch 
+                        {
+                            
+                        }
+
+                        if(reply != null && reply.Status == IPStatus.Success)
+                        {
+                            string text;
+                            if(reply.RoundtripTime == 0)
+                            { 
+                                text = "< 1 ms";
+                            }
+                            else
+                            {
+                                text = reply.RoundtripTime.ToString() + " ms";    
+                            }
+
+                            BeginInvoke(new Action(()=> item.SubItems[1].Text = text));
+
+                            if(!triedGetName)
+                            {
+                                triedGetName = true;
+                                try
+                                {
+                                    var host = Dns.GetHostEntry(reply.Address);
+                                    BeginInvoke(new Action(() => item.SubItems[2].Text = "Hostname = " + host.HostName));
+                                }
+                                catch { }
+                            }
+
+                            if(reply.RoundtripTime < 1000)
+                            { 
+                                Thread.Sleep((int)((long)1000 - reply.RoundtripTime));
+                            }
+
+                            
+                        }
+                        else
+                        {
+                            BeginInvoke(new Action(()=> item.SubItems[1].Text = "n/a"));
+                            Thread.Sleep(1000);
+                        }
+                    }
+                }
+            });
+            thread.Start();
+        }
+
+        #region User input
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if(richTextBoxIp.Text != string.Empty)
+            {
+                var item = new ListViewItem(richTextBoxIp.Text);
+                item.SubItems.Add(new ListViewItem.ListViewSubItem(item, "n/a"));
+                item.SubItems.Add(new ListViewItem.ListViewSubItem(item, ""));
+                listView.Items.Add(item);
+                PingHostForever(richTextBoxIp.Text,item);
+
+                richTextBoxIp.Text = string.Empty;
+                this.ActiveControl = richTextBoxIp;
+            }           
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            if(listView.SelectedIndices.Count > 0)
+            { 
+                var item = listView.Items[listView.SelectedIndices[0]];                
+                listView.Items.RemoveAt(listView.SelectedIndices[0]);
+            }
+        }
+
+   
+        #endregion
+
+      
+
+        private void richTextBoxIp_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            { 
+                buttonAdd_Click(sender,EventArgs.Empty);
+            }
+        }
+
+        private void buttonScanSubnet_Click(object sender, EventArgs e)
+        {
+            IPAddress localAdress;
+            IPAddress subnetMask;
+            var success = GetLocalSubnetMask(out localAdress, out subnetMask);
+            if(success)
+            {
+                listView.Items.Clear();
+                StartScan(localAdress,subnetMask);
+            }
+        } 
+
+        private bool GetLocalSubnetMask(out IPAddress localAdress, out IPAddress subnetMask)
+        {
+            localAdress = null;
+            subnetMask = null;
+
+            try
+            {
+                NetworkInterface defaultInterface = null;                
+                localAdress = null;
+                subnetMask = null;
+
+                foreach(NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    if(nic.OperationalStatus == OperationalStatus.Up)
+                        if((nic.Speed == 100000000) || (nic.Speed == 1000000000))
+                            if(nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                            {
+                                defaultInterface = nic;
+                                break;
+                            }
+                }
+
+                if(defaultInterface != null)
+                {
+                    foreach(var unicastIPAddressInformation in defaultInterface.GetIPProperties().UnicastAddresses)
+                    {
+                        if(unicastIPAddressInformation.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            localAdress = unicastIPAddressInformation.Address;
+                            subnetMask = unicastIPAddressInformation.IPv4Mask;
+                            break;
+                        }
+                    }
+                }
+                else return false;
+
+                if(subnetMask != null)
+                { 
+                    return true;
+                }
+                else return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void StartScan(IPAddress localAdress, IPAddress subnetMask)
+        {
+            var addressBytes = localAdress.GetAddressBytes();
+            var maskBytes = subnetMask.GetAddressBytes();
+            var address = new byte[4];
+
+            uint hostCount = (uint)((byte)~maskBytes[0] << 24 | (byte)~maskBytes[1] << 16 | (byte)~maskBytes[2] << 8 | (byte)~maskBytes[3] << 0);
+            
+            for(int i = 0; i < address.Length; i++)
+            address[i] = (byte)(addressBytes[i] & maskBytes[i]);            
+
+            for(int i = 1; i < hostCount - 1; i++)
+            {                
+                var adr = (byte[])address.Clone();
+
+                var ip = "";
+
+                for(int x = 0; x < adr.Length; x++)
+                {
+                    var mask = (byte)(i >> (24 - x * 8));
+                    adr[x] = (byte)(adr[x] | mask);
+                    ip += adr[x];
+                    if(x + 1 < adr.Length)
+                    {
+                        ip += ".";
+                    }
+                }
+                
+                try
+                {
+                    var currentIp = IPAddress.Parse(ip);
+                    var thread = new Thread(() =>                    
+                    {
+                        try
+                        {
+                            using(var pinger = new Ping())
+                            {
+                                var reply = pinger.Send(currentIp,4000);
+                                if(reply.Status == IPStatus.Success)
+                                {
+                                    var item = new ListViewItem(currentIp.ToString());
+                                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, "" + reply.RoundtripTime));
+                                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, ""));
+                                    Invoke(new Action(()=> listView.Items.Add(item)));
+                                    PingHostForever(currentIp.ToString(), item);
+                                }
+                            }
+                        }
+                        catch
+                        {
+
+                        }
+                    });
+                    thread.Start();
+                    
+                }
+                catch 
+                {
+                
+                }
+                
+            }
+            MessageBox.Show("Test");
+        }
+
+    }
+}
